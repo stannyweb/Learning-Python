b5c550c56e7d962220109da95c9628422e0708c1
stannyweb <stannyweb@gmail.com>
2026-04-27 08:40:25 +0300

    Basic Introduction to Data Types.

---
+++ Basic 1.py
@@ -0,0 +1,46 @@
+# Integer
+my_int = 5
+print("Integer:", my_int)
+
+# float
+my_float = 5.5
+print("float:", my_float)
+
+# Booleans
+my_boolean = True
+print("Boolean:", my_boolean)
+
+# Set
+my_set_values = {7, "Apple", 0.5}
+print("Set Values:", my_set_values)
+
+# Dictionary
+my_dictionary = {"name": "Stan", "age": 30}
+print("Dictionary:", my_dictionary)
+
+# Tuple
+my_tuple = (1, "Stan", 4)
+print("Tuple:", my_tuple)
+
+# Range
+my_range = range(10)
+print("Range:", my_range)
+
+# List
+my_list = ["apple", 24, "cherry"]
+my_list[1] = "Stan"
+my_list.insert(0, "banana")
+print("List:", my_list)
+
+# None
+my_none = None
+print("None:", my_none)
+
+# Type
+print(type(my_none))
+print(type(my_list))
+
+# isInstance
+print(isinstance(my_int, int))
+print(isinstance(my_float, float))
+print(isinstance(my_boolean, bool))
